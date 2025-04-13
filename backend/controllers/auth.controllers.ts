4e3f2d0c2817838389977c3053ece46a5cc4b53b
utkbkts <utkbktss5@gmail.com>
2025-04-14 00:09:35 +0300

    feat : signup

--- backend/controllers/auth.controllers.ts
+++ backend/controllers/auth.controllers.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/error.handler";
 import { pool } from "../libs/database";
+import { hashPassword } from "../libs/token";
 
 const signUpUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { firstName, email, password } = req.body;
+    const { firstname, email, password } = req.body;
 
-    if (!firstName || !email || !password) {
+    if (!firstname || !email || !password) {
       return next(new ErrorHandler("Provide Required Fields!", 404));
     }
 
@@ -15,17 +16,35 @@ const signUpUser = async (req: Request, res: Response, next: NextFunction) => {
       values: [email],
     });
 
-    if (user_exists.rows[0].user_exists) {
+    if (user_exists.rows[0].exists) {
       return next(
         new ErrorHandler("Email address already exists. Try Login", 409)
       );
     }
+
+    const hashedPassword = await hashPassword(password);
+
+    const insertResult = await pool.query({
+      text: `INSERT INTO tbluser (firstname,email,password) VALUES ($1,$2,$3) RETURNING *`,
+      values: [firstname, email, hashedPassword],
+    });
+
+    const newUser = { ...insertResult.rows[0] };
+    delete newUser.password;
+
+    res.status(201).json({
+      status: "Success",
+      message: "User account created successfully",
+      user: newUser,
+    });
   } catch (error) {
     return next(new ErrorHandler("Internal server Error", 500));
   }
 };
 
-const signInUser = async (req: Request, res: Response) => {};
+const signInUser = async (req: Request, res: Response) => {
+    
+};
 
 export default {
   signUpUser,
